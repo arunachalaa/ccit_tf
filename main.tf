81d7e1610982dcfeaf94488ca40f5fec309494be
root <root@ip-172-31-85-158.ec2.internal>
2024-02-21 02:06:50 +0000

    Initial commit

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample" {
   ami           = var.ami
   instance_type = var.instance_type
+  count = 2
   tags = {
     Name = "SampleTF_Instance"
   }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "instance_type" {
-  default = "t2.medium"
+  default = "t2.nano"
 }
 variable "ami" {
   default = "ami-0cf10cdf9fcd62d37"  # Replace with the appropriate AMI ID
